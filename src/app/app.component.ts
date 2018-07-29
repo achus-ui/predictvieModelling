4b44ea0693599649e31e7322fc08464ee210a1b0
unknown <harikrishnan@skiploop.com>
2018-07-29 19:51:24 +0530

    changes pushing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SolutionService } from './shared/solution.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private solutionServie: SolutionService) {}
+
+  ngOnInit() {
+    if(this.solutionServie.solutionDetails.name === '') {
+      this.router.navigate(['']);
+    }
+  }
+  
 }
--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,18 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { SolutionService } from '../shared/solution.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html'
 })
-export class BreadcrumbsComponent implements OnInit {
-
+export class BreadcrumbsComponent implements OnInit, DoCheck {
+  
+  public currentRoute: string = '';
   public getSolutionName: string = '';
 
-  constructor(private solutionService: SolutionService) { }
+  constructor(
+    private solutionService: SolutionService,
+    private location: Location) { }
 
   ngOnInit() {
     this.getSolutionName = this.solutionService.solutionDetails.name;
   }
+
+  ngDoCheck() {
+    switch (this.location.path()) {
+      case '/solution-details':
+        this.currentRoute = 'Solution Description'
+        break;
+
+      case '/business-variables':
+        this.currentRoute = 'Variables'
+        break;
+
+      case '/feature-engineering':
+        this.currentRoute = 'Feature Engineering'
+        break;
+
+      case '/model-technique':
+        this.currentRoute = 'Model Technique'
+        break;
+
+      default:
+        this.currentRoute = ''
+        break;
+    }
+  }
+
 
 }
